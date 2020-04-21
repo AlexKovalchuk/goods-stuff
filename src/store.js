877f0594ae027224c5f366b8052d5a9b24f55b4b
AlexKovalchuk <songossj372@gmail.com>
2020-04-21 17:04:43 +0300

    devtools

--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import {createStore, compose, applyMiddleware} from 'redux';
 import rootReducer from './reducers';
 import {createBrowserHistory} from 'history';
 import {routerMiddleware} from 'connected-react-router';
+import { devToolsEnhancer } from "redux-devtools-extension/logOnlyInProduction";
 import createSagaMiddleware from 'redux-saga';
 import {saga} from "./sagas";
 
@@ -9,7 +10,7 @@ import {saga} from "./sagas";
 const sagaMiddleware = createSagaMiddleware();
 
 const initialState = {};
-const enhancers = [];
+const enhancers = [devToolsEnhancer()];
 export const history = createBrowserHistory();
 
 const composedEnhancers = compose(
